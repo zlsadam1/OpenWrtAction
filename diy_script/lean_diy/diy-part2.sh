070c5e2e76b39c13f4fd1f4e9494827e47b0cf80
zlsadam1 <54363437+zlsadam1@users.noreply.github.com>
2026-07-01 14:02:59 +0800

    Update diy-part2.sh

--- diy_script/lean_diy/diy-part2.sh
+++ diy_script/lean_diy/diy-part2.sh
@@ -28,7 +28,7 @@ fi
 rm -rf ./feeds/packages/net/open-app-filter
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.10.0.253/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # Modify default passwd
 sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./ d' package/lean/default-settings/files/zzz-default-settings
